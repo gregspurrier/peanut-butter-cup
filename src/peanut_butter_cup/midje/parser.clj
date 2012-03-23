33dfe59d15ee1b9f0aaba9b1d62b32c1747aea5c
Greg Spurrier <gspurrier@linkedin.com>
2012-03-23 14:04:35 -0700

    Add support for doc strings

--- src/peanut_butter_cup/midje/parser.clj
+++ src/peanut_butter_cup/midje/parser.clj
@@ -1,6 +1,7 @@
 (ns peanut-butter-cup.midje.parser
   (:use [clojure.core.match :only (match)]
-        [clojure.pprint :only (pprint)]))
+        [clojure.pprint :only (pprint)])
+  (:require [clojure.string :as str]))
 
 (defn- ^:testable form-seq
   "Returns the forms read from rdr as a lazy sequence. rdr must implement
@@ -15,21 +16,68 @@ java.io.PushbackReader"
   (and (string? s)
        (boolean (re-matches #"^about\s+.+" s))))
 
+(defn- ^:testable fn-name-from-about
+  "Extracts the name of the function, as a symbol, from an about string"
+  [s]
+  (symbol (nth (str/split s #"\s+") 1)))
+
+(defn- doc-str?
+  [x]
+  (and (map? x) (contains? x :doc)))
+
+(defn- expectation?
+  [x]
+  (and (map? x) (contains? x :lhs)))
+
+(defn- prereq?
+  [x]
+  (and (map? x) (contains? x :prereqs)))
+
 (defn- parse-prereq-clauses
   [ps]
   (match [ps]
          [([lhs '=> rhs & more] :seq)] (cons {:lhs lhs :rhs rhs}
                                              (parse-prereq-clauses more))))
 
-(defn- parse-fact-clauses
+(defn- ^:testable extract-fact-clauses
+  "Given the tail of a fact expression, extracts the clauses that peanut butter
+cup cares about:
+  - doc strings
+  - top-level expectations
+  - prerequisites
+Other clauses are ignored."
   [cs]
   (match [cs]
-         [([lhs '=> rhs (['provided & prereqs] :seq) & more] :seq)]
-         (cons {:lhs lhs :rhs rhs :prereqs (parse-prereq-clauses prereqs)}
-               (parse-fact-clauses more))
-               
-         [([lhs '=> rhs & more] :seq)] (cons {:lhs lhs :rhs rhs}
-                                             (parse-fact-clauses more))))
+         [([(s :when string?) & more] :seq)]
+         (cons {:doc s} (extract-fact-clauses more))
+         [([lhs '=> rhs & more] :seq)]
+         (cons {:lhs lhs :rhs rhs} (extract-fact-clauses more))
+         [([(['provided & clauses] :seq) & more] :seq)]
+         (cons {:prereqs (parse-prereq-clauses clauses)}
+               (extract-fact-clauses more))
+         [([_ & more] :seq)] (if (seq more) (extract-fact-clauses more))))
+
+(defn- ^:testable combine-fact-clauses
+  "Combines a seq of fact clauses such that expectations are merged with doc
+strings that immediately precede them and prerequisites that immediately
+follow them. Doc strings and prerequisites that are found in unexpected
+posisitons are ignored."
+  [cs]
+  (match [cs]
+         [([(d :when doc-str?) (e :when expectation?) (p :when prereq?) & more] :seq)]
+         (cons (merge d e p) (combine-fact-clauses more))
+         [([(d :when doc-str?) (e :when expectation?) & more] :seq)]
+         (cons (merge d e) (combine-fact-clauses more))
+         [([(e :when expectation?) (p :when prereq?) & more] :seq)]
+         (cons (merge e p) (combine-fact-clauses more))
+         [([(e :when expectation?) & more] :seq)]
+         (cons e (combine-fact-clauses more))
+         [([_ & more] :seq)]
+         (if (seq more) (combine-fact-clauses more))))
+
+(defn- parse-fact-clauses
+  [cs]
+  (-> cs extract-fact-clauses combine-fact-clauses))
 
 (defn parse-fact-form
   "Parses a Midje fact form and extracts a sequence of fact maps. Each map
@@ -38,6 +86,6 @@ side of the fact, respectively."
   [fact]
   (match [fact]
          [([(:or 'fact 'facts) (s :when about-string?) & clauses] :seq)]
-         {:doc s :clauses (parse-fact-clauses clauses)}
+         {:about (fn-name-from-about s) :clauses (parse-fact-clauses clauses)}
          [([(:or 'fact 'facts) & clauses] :seq)]
          {:clauses (parse-fact-clauses clauses)}))
--- test/peanut_butter_cup/test/midje/parser.clj
+++ test/peanut_butter_cup/test/midje/parser.clj
@@ -14,16 +14,50 @@
 
 (facts "about about-string?"
   (about-string? "about foo")
-  => true)
+  => true
+  (about-string? "something else")
+  => false
+  (about-string? '(x y z))
+  => false)
+
+(facts "about fn-name-from-about"
+  (fn-name-from-about "about make-monkey")
+  => 'make-monkey)
+
+(facts "about extract-fact-caluses"
+  (extract-fact-clauses ["hello"])
+  => '({:doc "hello"})
+  (extract-fact-clauses '(..lhs.. => ..rhs..))
+  => '({:lhs ..lhs.. :rhs ..rhs..})
+  (extract-fact-clauses '((provided ..lhs.. => ..rhs..)))
+  => '({:prereqs ({:lhs ..lhs.. :rhs ..rhs..})}))
+
+(facts "about combine-fact-clauses"
+  (combine-fact-clauses [{:doc ..d..} {:lhs ..l..} {:prereqs ..p..}])
+  => '({:doc ..d.. :lhs ..l.. :prereqs ..p..})
+  (combine-fact-clauses [{:doc ..d..} {:lhs ..l..}])
+  => '({:doc ..d.. :lhs ..l..})
+  (combine-fact-clauses [{:lhs ..l..} {:prereqs ..p..}])
+  => '({:lhs ..l.. :prereqs ..p..})
+  (combine-fact-clauses [{:lhs ..l..}])
+  => '({:lhs ..l..})
+  (combine-fact-clauses [{:lhs ..l1..} {:lhs ..l2..}])
+  => '({:lhs ..l1..} {:lhs ..l2..})
+  (combine-fact-clauses [{:doc ..d..} {:lhs ..l1..} {:lhs ..l2..} {:prereqs ..p..}])
+  => '({:doc ..d.. :lhs ..l1..} {:lhs ..l2.. :prereqs ..p..}))
 
 (comment
   ;; Pending resolution of https://github.com/marick/Midje/issues/117
   (facts "about parse-fact-form"
     (parse-fact-form '(fact ..lhs.. => ..rhs..))
     => {:clauses [{:lhs ..lhs.. :rhs ..rhs..}]}
-    (parse-fact-form '(fact ..lhs1.. => ..rhs1.. ..lhs2.. => ..rhs2..))
+    (parse-fact-form '(fact "about some-fn" ..lhs.. => ..rhs..))
+    => {:about 'some-fn :clauses [{:lhs ..lhs.. :rhs ..rhs..}]}
+    (parse-fact-form '(fact ..lhs1.. => ..rhs1..
+                        "one more thing"
+                        ..lhs2.. => ..rhs2..))
     => {:clauses [{:lhs ..lhs1.. :rhs ..rhs1..}
-                   {:lhs ..lhs2.. :rhs ..rhs2..}]}
+                   {:doc "one more thing" :lhs ..lhs2.. :rhs ..rhs2..}]}
     (parse-fact-form '(fact ..lhs.. => ..rhs..
                         (provided ..p-lhs.. => ..p-rhs..)))
     => {:clauses [{:lhs ..lhs1.. :rhs ..rhs1.. :prereqs [{:lhs ..p-lhs..
