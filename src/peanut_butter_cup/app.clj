ba6c947274869f5bf094a5d1282c9bdc00bb5c0b
Greg Spurrier <greg.spurrier@gmail.com>
2012-03-25 18:19:17 -0700

    Emit HTML

--- src/peanut_butter_cup/app.clj
+++ src/peanut_butter_cup/app.clj
@@ -1,10 +1,13 @@
 (ns peanut-butter-cup.app
   (:require [peanut-butter-cup.core :as core]
+            [peanut-butter-cup.templates.namespace :as ns-template]
             [clojure.pprint :as pp]))
 
 (defn -main
   [& args]
   (doseq [arg args]
-    (let [name (symbol arg)]
-      (require name)
-      (pp/pprint (core/get-fn-metadata-and-facts name)))))
+    (let [ns (symbol arg)]
+      (require ns)
+      (println (apply str (ns-template/template
+                           ns
+                           (core/get-fn-metadata-and-facts ns)))))))
